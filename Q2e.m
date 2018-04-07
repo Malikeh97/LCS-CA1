51629156a7307b3c9f7bc9b9aa27f53d792ebd9b
malikeh ehghaghi <malikehehghaghi@gmail.com>
2018-04-07 12:47:29 +0430

    Q6 and 7 corrected

--- Q2e.m
+++ Q2e.m
@@ -7,10 +7,24 @@ m1 = 466.5;
 s = tf('s');
 G2 = (-m1*b2*s^3-m1*k2*s^2)/((m1*s^2+b1*s+k1)*(m2*s^2+(b1+b2)*s+(k1+k2))-(b1*s+k1)*(b1*s+k1));
 num = [-b2/m2 -k2/m2 0 0];
-den = [1 ((b1/m2)+(b2/m2)+(b1/m1)) ((k1/m2)+(k2/m1)+((b1*b2)/(m1*m2))+(k1/m1)) (((b1*k2)/(m1*m2))+((b2*k1)/(m1*m2))) (k1*k2)/(m1*m2)];
+den = [1 ((b1/m2)+(b2/m2)+(b1/m1)) ((k1/m2)+(k2/m2)+((b1*b2)/(m1*m2))+(k1/m1)) (((b1*k2)/(m1*m2))+((b2*k1)/(m1*m2))) (k1*k2)/(m1*m2)];
 [A,B,C,D] = tf2ss(num,den);
 csys = ss(A,B,C,D);
-subplot(2,1,1);
-step(csys);
-subplot(2,1,2);
-impulse(csys);
+%subplot(2,1,1);
+[y,t,x] = step(csys);
+figure(1);
+for i = 1:4
+    subplot(2,2,i);
+    plot(t,x(:,i));
+    xlabel('time')
+    ylabel(strcat('x', num2str(i)));
+end
+%subplot(2,1,2);
+[y,t,x] = impulse(csys);
+figure(2);
+for i = 1:4
+    subplot(2,2,i);
+    plot(t,x(:,i));
+    xlabel('time')
+    ylabel(strcat('x', num2str(i)));
+end
